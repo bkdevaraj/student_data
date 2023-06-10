b3f7f69a0b4ccb982c59e13a34624bbb03e761b4
bkdevaraj <creativecreatstudio@gmail.com>
2023-06-10 15:33:59 +0530

    time

--- script/update_student_class.py
+++ script/update_student_class.py
@@ -25,7 +25,7 @@ def update_student_class():
     now = datetime.now()
 
     # Check if it's March 21 at 12:18 PM
-    if now.month == 6 and now.day == 10 and now.hour == 15 and now.minute == 30:
+    if now.month == 6 and now.day == 10 and now.hour == 15 and now.minute == 37:
         
         # Update the student class
         students = Student.objects.all()
